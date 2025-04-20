5131cd91ca5c1c4dcf5291a50050aa128a7b4f97
Indhu Botcha <botchaindravathi248@gmail.com>
2025-04-20 11:40:00 +0530

    fix: add vercel rewrite rules for react

--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -60,7 +60,7 @@ const Navbar = () => {
               }}
               key={index}
               className="text-sm text-black hover:text-[#C3FF6A] cursor-pointer transition"
-            href={link.link}>{link.name}</a>
+            >{link.name}</a>
             </li>
           ))}
         </ul>
